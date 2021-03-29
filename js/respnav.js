ceb8b9ba07eaf037a3872c04f2f53c4538715422
arnaudpauline <70948234+arnaudpauline@users.noreply.github.com>
2021-03-29 14:56:22 +0200

    Add files via upload

--- js/respnav.js
+++ js/respnav.js
@@ -1,3 +1,4 @@
+//navbar responsive
 function myFunction() {
     var x = document.getElementById("mysidebar");
     var screen = document.getElementById("screen");
@@ -16,3 +17,39 @@ function myFunction() {
   }
 
 
+// modal pour photos
+  //Amiciel img formulaire
+var modal1 = document.getElementById("myModal1");
+                  
+var img1 = document.getElementById("myImg1");
+var modalImg1 = document.getElementById("imgViewF");
+var captionText1 = document.getElementById("caption1");
+img1.onclick = function(){
+  modal1.style.display = "block";
+  modalImg1.src = this.src;
+  captionText1.innerHTML = this.alt;
+}
+
+var span1 = document.getElementsByClassName("close")[0];
+
+span1.onclick = function() { 
+  modal1.style.display = "none";
+}
+
+  //Amiciel img tableau
+var modal2 = document.getElementById("myModal2");
+                 
+var img2 = document.getElementById("myImg2");
+var modalImg2 = document.getElementById("imgViewT");
+var captionText2 = document.getElementById("caption2");
+img2.onclick = function(){
+  modal2.style.display = "block";
+  modalImg2.src = this.src;
+  captionText2.innerHTML = this.alt;
+}
+  
+var span2 = document.getElementsByClassName("close")[1];
+
+span2.onclick = function() { 
+  modal2.style.display = "none";
+}
